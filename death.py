ae497dba3ab91591ee8bdd2a1239d28c03ed7aa2
VatsalAgarwal <vatsal0298@gmail.com>
2017-06-02 05:27:35 +0530

    Update death.py
    Script that reads the latest entry on the US Supreme Court webpage and returns the number of times there was laughter in the court.
    Solved as exercise from '101 Web Scraping Tutorials'

--- death.py
+++ death.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+# URL for the US Supreme Court Website :
+
 url = requests.get("https://www.tdcj.state.tx.us/death_row/dr_offenders_on_dr.html")
 
 soup = bs(url.content, 'html.parser')
